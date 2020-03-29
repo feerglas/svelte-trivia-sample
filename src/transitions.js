d6bfb25b924c03afc083d5986e53f5cc75741e07
feerglas <info@feerglas.com>
2020-03-29 09:09:43 +0200

    Initial commit

---
+++ src/transitions.js
@@ -0,0 +1,20 @@
+import { sineOut } from 'svelte/easing';
+
+const duration = 250;
+const delay = duration;
+
+const delayZero = 0;
+
+export const fadeIn = () => ({
+  duration,
+  delay,
+  easing: sineOut,
+  css: t => `opacity: ${t}`
+});
+
+export const fadeOut = () => ({
+  duration,
+  delayZero,
+  easing: sineOut,
+  css: t => `opacity: ${t}`
+});
